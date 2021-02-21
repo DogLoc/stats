a740f123ce4e7a51640acafef4a60a23370f1941
DogLoc <enzo.69@outlook.fr>
2021-02-21 21:39:14 +0100

    add api information onclick country

--- src/componants/body/map/ColorPicker.js
+++
@@ -1,8 +0,0 @@
-  
-import React from "react";
-import { GithubPicker } from "react-color";
-
-const ColorPicker = ({ color, handleColorChange }) => (
-  <GithubPicker color={color} onChangeComplete={handleColorChange} />
-);
-export default ColorPicker;
--- src/componants/body/map/Map.js
+++ src/componants/body/map/Map.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { VectorMap } from "react-jvectormap";
-import ColorPicker from "./ColorPicker";
 import '../../../css/map.scss';
 
 import { jsx } from "@emotion/core";
+import { Popover } from '@varld/popover';
 import styled from "@emotion/styled";
 
 const { getName } = require("country-list");
@@ -13,6 +13,7 @@ class Map extends React.Component {
     countriesCodesArray: [],
     countriesNamesArray: [],
     data: {},
+    dataCovid: {},
     title: "",
     titleSet: false,
     color: "#48aeef"
@@ -25,7 +26,7 @@ class Map extends React.Component {
 
   handleClick = (e, countryCode) => {
     const { countriesCodesArray } = this.state;
-    // console.log(countryCode);
+    //console.log(countryCode);
     if (countriesCodesArray.indexOf(countryCode) === -1) {
       this.setState(
         {
@@ -38,13 +39,33 @@ class Map extends React.Component {
 
   getCountriesNamesList = () => {
     const { countriesCodesArray } = this.state;
-    const list = countriesCodesArray.map(code => getName(code));
-    this.setState(
-      {
-        countriesNamesArray: list
-      },
-      () => this.makeMapDataStructure()
-    );
+    const list = countriesCodesArray.map(code =>
+      
+      fetch(`https://coronavirus-map.p.rapidapi.com/v1/summary/region?region=${getName(code)}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "9ad6965570msh263b6eabf12b69fp1ae439jsn82a313ffee62",
+        "x-rapidapi-host": "coronavirus-map.p.rapidapi.com"
+      }
+    })
+      .then(response => {
+        const promiseCovid = response.json(Promise.resolve());
+
+        promiseCovid.then((value) => {
+          const dataCovid = value.data.summary;
+          this.setState({
+            isLoaded: true,
+            dataCovid: dataCovid,
+            countriesNamesArray: list 
+          },
+          () => this.makeMapDataStructure()
+          );
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      })  
+      );
   };
 
   makeMapDataStructure = () => {
@@ -57,9 +78,11 @@ class Map extends React.Component {
     });
   };
 
+
+
+
   render() {
-    const { countriesNamesArray, data, color } = this.state;
-    // console.log(data)
+    const { countriesNamesArray, data, dataCovid, color } = this.state;
     return (
       <div>
         <VectorMap
@@ -74,26 +97,18 @@ class Map extends React.Component {
           containerClassName="map"
           regionStyle={{
             initial: {
-              fill: "#e4e4e4",
-              "fill-opacity": 0.9,
-              stroke: "none",
-              "stroke-width": 0,
-              "stroke-opacity": 0
+              fill: "#e0e0e0"
             },
             hover: {
-              "fill-opacity": 0.8,
-              cursor: "pointer"
-            },
-            selected: {
-              fill: "#2938bc" // color for the clicked country
-            },
-            selectedHover: {}
+              fill: "#870000",
+              cursor: 'pointer'
+            }
           }}
-          regionsSelectable={true}
+          regionsSelectable={false}
           series={{
             regions: [
               {
-                values: data, // this is the map data
+                values: dataCovid, // this is the map data
                 scale: ["#146804", color], // your color game's here
                 normalizeFunction: "polynomial"
               }
@@ -107,17 +122,4 @@ class Map extends React.Component {
 
 export default Map;
 
-const Container = styled.div`
-  text-align: center;
-  input {
-    padding: 10px;
-    border-radius: 5px;
-    border-shadow: 0;
-    border-style: solid;
-    font-size: 16px;
-    &:focus {
-      outline: none;
-    }
-  }
-`;
 
